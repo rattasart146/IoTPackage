594319285da80141d59ced114b4db925df6128dc
rattasart146 <59070146@it.kmitl.ac.th>
2018-12-15 03:40:01 +0700

    Completed : Non Correctly

--- LCD-SerialInput/LCD-SerialInput.ino
+++ LCD-SerialInput/LCD-SerialInput.ino
@@ -1,19 +1,21 @@
 
+
 #define BLYNK_PRINT Serial
+
+#include <LiquidCrystal.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <BlynkSimpleEthernet.h>
-#include <LiquidCrystal.h>
+
 
 char auth[] = "5daa5e9d94764019ae49e72a58e78d2d";
-#define W5100_CS  10
-#define SDCARD_CS 4
-
 char ch;
 int Contrast=15;
 // initialize the library with the numbers of the interface pins
-WidgetLCD lcdVirtual(V1);
+
 LiquidCrystal lcdReal(12, 11, 5, 4, 3, 2);
+WidgetLCD lcdVirtual(V1);
+
 const int trigPin = 8;
 const int echoPin = 7;
 // defines variables
@@ -23,11 +25,8 @@ int distance;
 void setup() 
 {
   Serial.begin(9600);
-
-  pinMode(SDCARD_CS, OUTPUT);
-  digitalWrite(SDCARD_CS, HIGH); // Deselect the SD card
-  Blynk.begin(auth, "blynk-cloud.com", 8442);
   
+  Blynk.begin(auth, "blynk-cloud.com", 8442);
   Serial.println("LCD test with PWM contrast adjustment");
   pinMode(13,OUTPUT);
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
@@ -35,9 +34,11 @@ void setup()
   analogWrite(6,Contrast);
   // set up the LCD's number of columns and rows: 
   // Print a message to the LCD.
+
+  lcdReal.begin(16, 2);
   lcdReal.setCursor(0, 0);
   lcdReal.print("Distance: ");
-  lcdVirtual.print(0, 0, "Distance: ");
+  
 }
 
 void loop() 
@@ -55,30 +56,24 @@ void loop()
   distance= duration*0.034/2;
   // Prints the distance on the Serial Monitor
   Serial.print("Distance: ");
-  Serial.println(distance);
+  Serial.print(distance);
+  Serial.println(" CM");
 
   
-   digitalWrite(13,LOW);
-   delay(1000);
-   digitalWrite(13,HIGH);
   // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcdReal.setCursor(0, 1);
-  lcdReal.print(distance);
-  lcdVirtual.clear();
-  // print the number of seconds since reset:
-  lcdVirtual.print(0, 1,distance);
-  lcdVirtual.clear();
-  
-  Blynk.run();
-}
+  lcdReal.setCursor(0, 0);
+  lcdReal.print("Distance: ");
+  lcdReal.println(distance);
+  lcdReal.setCursor(14, 0);
+  lcdReal.print("Cm");
 
-void serialEvent()
-{
-     if (Serial.available())
-      {
-        analogWrite(6,"mew");
-        Serial.println("Current contrast");
-        Serial.println(Contrast);
-      }
+  lcdVirtual.print(0, 0, "Distance: ");
+  lcdVirtual.print(10, 0, "    ");
+  lcdVirtual.print(10, 0, distance);
+  lcdVirtual.print(14, 0, "Cm");
+
+  delay(500);
+  // print the number of seconds since reset:
+  Blynk.run();
+  
 }
