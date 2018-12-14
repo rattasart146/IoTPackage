733d9aeac8502610f1bcf7091a2cba5dee670994
rattasart146 <59070146@it.kmitl.ac.th>
2018-12-15 01:52:25 +0700

    Add Ultrasonic to LCD monitor

--- LCD-SerialInput/LCD-SerialInput.ino
+++ LCD-SerialInput/LCD-SerialInput.ino
@@ -1,24 +1,49 @@
 
 #include <LiquidCrystal.h>
+
 char ch;
 int Contrast=15;
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int trigPin = 8;
+const int echoPin = 7;
+// defines variables
+long duration;
+int distance;
 
 void setup() 
 {
   Serial.begin(9600);
+  
   Serial.println("LCD test with PWM contrast adjustment");
   pinMode(13,OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   analogWrite(6,Contrast);
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("LCD test!!");
+  lcd.print("Distance: ");
 }
 
 void loop() 
 {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(1000);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW); 
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  
    digitalWrite(13,LOW);
    delay(1000);
    digitalWrite(13,HIGH);
@@ -26,32 +51,6 @@ void loop()
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(millis()/1000);
-}
-
-void serialEvent()
-{
-     if (Serial.available())
-  {
-    ch= Serial.read();
-    if(ch=='A' && Contrast<255)
-    {
-      Contrast=Contrast+1;
-    }
-    if(ch=='B' && Contrast>0)
-    {
-      Contrast=Contrast-1;
-    }
-        if(ch=='N')
-    {
-      analogWrite(9,28836);
-    }
-       if(ch=='F')
-    {
-      analogWrite(9,0);
-    }
-    analogWrite(6,Contrast);
-    Serial.println("Current contrast");
-    Serial.println(Contrast);
-  }
+  lcd.print(distance);
+  lcd.print("                ");
 }
