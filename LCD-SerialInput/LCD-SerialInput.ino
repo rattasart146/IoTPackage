daadca29ad6e03379690f2dca442619b298f207a
rattasart146 <59070146@it.kmitl.ac.th>
2018-12-15 02:21:46 +0700

    Add System to Display App control

--- LCD-SerialInput/LCD-SerialInput.ino
+++ LCD-SerialInput/LCD-SerialInput.ino
@@ -1,10 +1,19 @@
 
+#define BLYNK_PRINT Serial
+#include <SPI.h>
+#include <Ethernet.h>
+#include <BlynkSimpleEthernet.h>
 #include <LiquidCrystal.h>
+
+char auth[] = "5daa5e9d94764019ae49e72a58e78d2d";
+#define W5100_CS  10
+#define SDCARD_CS 4
 
 char ch;
 int Contrast=15;
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+WidgetLCD lcdVirtual(V1);
+LiquidCrystal lcdReal(12, 11, 5, 4, 3, 2);
 const int trigPin = 8;
 const int echoPin = 7;
 // defines variables
@@ -14,6 +23,10 @@ int distance;
 void setup() 
 {
   Serial.begin(9600);
+
+  pinMode(SDCARD_CS, OUTPUT);
+  digitalWrite(SDCARD_CS, HIGH); // Deselect the SD card
+  Blynk.begin(auth, "blynk-cloud.com", 8442);
   
   Serial.println("LCD test with PWM contrast adjustment");
   pinMode(13,OUTPUT);
@@ -21,9 +34,10 @@ void setup()
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   analogWrite(6,Contrast);
   // set up the LCD's number of columns and rows: 
-  lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("Distance: ");
+  lcdReal.setCursor(0, 0);
+  lcdReal.print("Distance: ");
+  lcdVirtual.print(0, 0, "Distance: ");
 }
 
 void loop() 
@@ -49,8 +63,22 @@ void loop()
    digitalWrite(13,HIGH);
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
+  lcdReal.setCursor(0, 1);
+  lcdReal.print(distance);
+  lcdVirtual.clear();
   // print the number of seconds since reset:
-  lcd.print(distance);
-  lcd.print("                ");
+  lcdVirtual.print(0, 1,distance);
+  lcdVirtual.clear();
+  
+  Blynk.run();
+}
+
+void serialEvent()
+{
+     if (Serial.available())
+      {
+        analogWrite(6,"mew");
+        Serial.println("Current contrast");
+        Serial.println(Contrast);
+      }
 }
